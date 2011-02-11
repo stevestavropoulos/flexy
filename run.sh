c8a6c29f2a66eb74d5a114eea111babea9b6f4a3
Steve Stavropoulos <steve@math.upatras.gr>
2011-02-11 20:28:07 +0200

    no dups

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@ isoconvert='iconv -f utf-8 -t iso-8859-7'
 utfconvert='iconv -t utf-8 -f iso-8859-7'
 mkdir -p .stage/
 
-cat flexy.py | sed -e '1,10s/utf-8/iso-8859-7/' | $isoconvert > .stage/flexy.py
-cat greek.py | sed -e '1,10s/utf-8/iso-8859-7/' | $isoconvert > .stage/greek.py
+for i in flexy.py greek.py; do
+	cat $i | sed -e '1,10s/utf-8/iso-8859-7/' | $isoconvert > .stage/$i
+done
 
 cd .stage/
 chmod 755 flexy.py
